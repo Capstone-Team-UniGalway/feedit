69ff970efcf3303681abf4590f733c409e3e2476
pinco227 <p4ul.gg@gmail.com>
2025-03-14 15:16:58 +0000

    chore: set up environment switching and basic env settings
    install required packages: django-environ for env management, whitenoise for serving static files to production, gunicorn for running django in production, dj-database-url for using DB URL instead of engine settings

--- feedit_app/app/urls.py
+++ feedit_app/app/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for feedit_app project.
+URL configuration for app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
