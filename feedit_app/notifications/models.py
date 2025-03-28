2f79f26764104c5b38913ea47d7539de0b9e25c8
pinco227 <p4ul.gg@gmail.com>
2025-03-28 12:36:50 +0000

    db: revise notifications and make migration

--- feedit_app/notifications/models.py
+++ feedit_app/notifications/models.py
@@ -1,15 +1,23 @@
 from django.conf import settings
 from django.db import models
+from app.base_model import BaseModel
+
+
 # Notification system for alerts and updates
-class Notification(models.Model):
+class Notification(BaseModel):
     NOTIFICATION_TYPES = [
-        ("review_alert", "Review Alert"),
-        ("thread_alert", "Thread Alert"),
-        ("employee_alert", "New Employee Alert"),
-        ("claim_alert", "Claim/Join Alert"),
+        ("new_review", "New Review"),
+        ("new_thread", "New Thread"),
+        ("new_thread_reply", "New Reply"),
+        ("join_request", "New Join Request"),
+        ("join_response", "New Join Response"),
+        ("claim_request", "New Claim Request"),
+        ("claim_request", "New Claim Response"),
+        ("general_request", "New Request"),
+        ("general_response", "New Response"),
     ]
     recipient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="notifications")
     type = models.CharField(max_length=20, choices=NOTIFICATION_TYPES)
     message = models.TextField()
-    is_read = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    action_url = models.URLField(null=True, blank=True)
+    read_at = models.DateTimeField(null=True, blank=True)
