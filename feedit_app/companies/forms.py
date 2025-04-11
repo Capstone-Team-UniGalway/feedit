d86af1981d35f79eccf905b1d4dde49259143049
pinco227 <p4ul.gg@gmail.com>
2025-04-11 14:11:07 +0100

    chore: create company form to be used in create and update company views

---
+++ feedit_app/companies/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from .models import Company
+from django.core.exceptions import ValidationError
+
+
+class CompanyForm(forms.ModelForm):
+    class Meta:
+        model = Company
+        fields = [
+            "name",
+            "industry",
+            "bio",
+            "country",
+            "city",
+            "date_founded",
+        ]
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "input input-bordered w-full"}),
+            "industry": forms.TextInput(attrs={"class": "input input-bordered w-full"}),
+            "bio": forms.Textarea(
+                attrs={"class": "textarea textarea-bordered w-full", "rows": 4}
+            ),
+            "country": forms.TextInput(attrs={"class": "input input-bordered w-full"}),
+            "city": forms.TextInput(attrs={"class": "input input-bordered w-full"}),
+            "date_founded": forms.DateInput(
+                attrs={"class": "input input-bordered w-full", "type": "date"}
+            ),
+        }
+
+    def clean_name(self):
+        name = self.cleaned_data.get("name", "")
+        if len(name) > 100:
+            raise ValidationError("Company name must be 100 characters or less.")
+        return name.strip()
+
+    def clean_industry(self):
+        industry = self.cleaned_data.get("industry", "")
+        return industry.strip()
