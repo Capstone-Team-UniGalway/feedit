f59ef8f41ccd406f969172f51da025a01cf8ef72
Glenn <glenny@DESKTOP-LNOVKJM.>
2025-04-22 22:09:03 +0200

    Add thread functionality and update dashboard to display threads and mentions

---
+++ feedit_app/threads/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django_ckeditor_5.widgets import CKEditor5Widget
+from .models import Thread
+
+
+class ThreadForm(forms.ModelForm):
+    """Form for creating and updating threads."""
+    
+    class Meta:
+        model = Thread
+        fields = ['title', 'content', 'type', 'visibility']
+        widgets = {
+            'content': CKEditor5Widget(
+                attrs={'class': 'django_ckeditor_5'}, 
+                config_name='extends'
+            )
+        }
+
+
+class ThreadReplyForm(forms.ModelForm):
+    """Form for creating replies to threads."""
+    
+    class Meta:
+        model = Thread
+        fields = ['content']
+        widgets = {
+            'content': CKEditor5Widget(
+                attrs={'class': 'django_ckeditor_5'}, 
+                config_name='extends'
+            )
+        }
